52065cb831ce2d55fa1e824cd46e29c01438d0eb
heidispsu <heidirxpharm@yahoo.com>
2015-06-14 20:29:21 -0400

    Initial Commit

---
+++ part3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  part3
+//
+//  Created by Heidi  on 6/14/15.
+//  Copyright (c) 2015 Heidi . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
